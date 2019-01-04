a4fabf34ba0758b3295076d8c21bf2f543af193a
jin <jinlu.hansome@163.com>
2019-01-04 14:07:48 +0800

    测试idea

--- src/mock/index.js
+++ src/mock/index.js
@@ -1,8 +1,8 @@
 import Mock from 'mockjs';
 import { getAuthorization, login } from './response/user';
 
-Mock.mock('/index/login', 'post', login);
-Mock.mock('/users/authorization', 'get', getAuthorization);
+Mock.mock('/api/login', 'post', login);
+Mock.mock('/api/authorization', 'get', getAuthorization);
 
 // 设置超时时间
 Mock.setup({
